dee8d68e5ccb0b2ab7301e76ca6d5a4e11957546
struhanets <posseydon87@gmail.com>
2024-09-16 15:28:41 +0200

    Solution

--- author/urls.py
+++ author/urls.py
@@ -3,9 +3,20 @@ from rest_framework import routers
 
 from author import views
 
+author_list = views.AuthorViewSet.as_view({
+    "get": "list",
+    "post": "create",
+})
+
+author_detail = views.AuthorViewSet.as_view({
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
+urlpatterns = [
+    path("manage/", author_list, name="manage-list"),
+    path("manage/<int:pk>/", author_detail, name="manage-detail"),
+]
+
 app_name = "author"
-
-router = routers.DefaultRouter()
-router.register("authors", views.AuthorViewSet)
-
-urlpatterns = [path("", include(router.urls))]
